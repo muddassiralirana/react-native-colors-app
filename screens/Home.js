ccbb432b5238501942772071f4a84f83f2ff05f9
muddassirAli <muddassiralirana786@gmail.com>
2021-12-02 17:14:37 +0500

    add pull to refresh

--- screens/Home.js
+++ screens/Home.js
@@ -1,94 +1,62 @@
 import React from 'react';
-import { useCallback, useEffect, useState  } from 'react';
+import {useCallback, useEffect, useState} from 'react';
 
-import {FlatList, StyleSheet} from 'react-native';
+import {FlatList, ScrollView, StyleSheet, View} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import PalettePreview from '../components/PalettePreview';
 
-
-// const COLORS = [
-//   { colorName: 'Base03', hexCode: '#002b36' },
-//   { colorName: 'Base02', hexCode: '#073642' },
-//   { colorName: 'Base01', hexCode: '#586e75' },
-//   { colorName: 'Base00', hexCode: '#657b83' },
-//   { colorName: 'Base0', hexCode: '#839496' },
-//   { colorName: 'Base1', hexCode: '#93a1a1' },
-//   { colorName: 'Base2', hexCode: '#eee8d5' },
-//   { colorName: 'Base3', hexCode: '#fdf6e3' },
-//   { colorName: 'Yellow', hexCode: '#b58900' },
-//   { colorName: 'Orange', hexCode: '#cb4b16' },
-//   { colorName: 'Red', hexCode: '#dc322f' },
-//   { colorName: 'Magenta', hexCode: '#d33682' },
-//   { colorName: 'Violet', hexCode: '#6c71c4' },
-//   { colorName: 'Blue', hexCode: '#268bd2' },
-//   { colorName: 'Cyan', hexCode: '#2aa198' },
-//   { colorName: 'Green', hexCode: '#859900' },
-// ];
-
-
-
 const Home = ({navigation}) => {
-  
-
   const [palettes, setPlettes] = useState([]);
-  const handleFetchPalettes = useCallback(
+  const [isRefreshing, setOIsRefreshing]= useState(false);
+  const handleFetchPalettes = useCallback(async () => {
+    const response = await fetch(
+      'https://color-palette-api.kadikraman.vercel.app/palettes',
+    );
+    if (response.ok) {
+      const palettes = await response.json();
+      setPlettes(palettes);
+    }
+  }, []);
+
+  useEffect(() => {
+    handleFetchPalettes();
+  }, []);
+
+  const handleRefresh = useCallback(
     async() => {
-     const response= await fetch("https://color-palette-api.kadikraman.vercel.app/palettes");
-     if (response.ok){
-       const palettes= await response.json();
-       setPlettes(palettes);
-     }
-    },
-    [],
-  )
-   useEffect(() => {
-    handleFetchPalettes()
-   
-   }, [])
+      
+      setOIsRefreshing(true);
+      await handleFetchPalettes();
+      setTimeout(()=>{
+        setOIsRefreshing(false)
+
+      },1000);
+    },[])
+
+ 
 
   return (
-    // <View>
-    //   <TouchableOpacity 
-      
-    //   onPress={()=>
-    //   navigation.push('ColorPalette',{
-    //     palettName:"Solarized",
-    //     color:COLORS,
-    //   })
-      
-    //   }>
-    //   <Text>Solarized</Text>
-
-    //   </TouchableOpacity>
-    // </View>
-
-
     <FlatList
-    data={palettes}
-    style={styles.list }
-    keyExtractor={item=>item.paletteName}
-    renderItem={({item})=>(
-      <PalettePreview
-  
-        onPress={()=>
-        navigation.push("ColorPalette",item)}
-      palette={item}
-       />
-         
-    
-
-    )}
-    
-    
+      data={palettes}
+      style={styles.list}
+      keyExtractor={item => item.paletteName}
+      renderItem={({item}) => (
+        <PalettePreview
+          onPress={() => navigation.push('ColorPalette', item)}
+          palette={item}
+        />
+      )}
+      refreshing={isRefreshing}
+      onRefresh={handleRefresh}
     />
   );
 };
 
-const styles=StyleSheet.create({
-  list:{
-    flex:1,
-    padding:10,
-    backgroundColor:"white",
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'white',
   },
 });
 
